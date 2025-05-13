9e81078bf2f5418d384afb0bb064cb2aa7cdf623
D@reK@r@n18 <xpresskaran98@gmail.com>
2025-05-13 16:48:59 +0530

    add listener
    Signed-off-by: D@reK@r@n18 <xpresskaran98@gmail.com>

---
+++ server/listener.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <memory>
+#include <iostream>
+
+#include "net.hpp"
+
+// Forward declaration
+class shared_state;
+
+// accepts incoming connections
+class listener : public std::enable_shared_from_this<listener> {
+	tcp::acceptor acceptor_;
+	tcp::socket socket_;
+	std::shared_ptr<shared_state> state_;
+
+	void fail(error_code ec, char const* what);
+	void on_accept(error_code ec);
+
+public:
+	listener(
+		net::io_context& ioc,
+		tcp::endpoint endpoint,
+		std::shared_ptr<shared_state> const& state
+	);
+
+	void run();
+};
